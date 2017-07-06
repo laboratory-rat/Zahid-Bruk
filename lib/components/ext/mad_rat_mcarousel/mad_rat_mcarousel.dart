311ee4ebfa93b61e99ef32adab84d33fe2c68d55
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-07-06 22:59:31 +0300

    fix

--- lib/components/ext/mad_rat_mcarousel/mad_rat_mcarousel.dart
+++ lib/components/ext/mad_rat_mcarousel/mad_rat_mcarousel.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:angular2/angular2.dart';
 
 @Component(
@@ -9,18 +10,59 @@ import 'package:angular2/angular2.dart';
 class MRMaterialCarousel{
     @Input()
     List<CarouselObject> objects;
-    
+
+    @Input()
+    int sleepTime = 5;
+
+
     CarouselObject get current => objects == null || objects.length < index ? null : objects[index]; 
     int index = 0;
+    bool isTriggered = false;
+    bool isMouseEnter = false;
+
+    MRMaterialCarousel(){
+        timer();
+    }
+
+    Future timer() async{
+        while(true){
+
+            await new Future.delayed(new Duration(seconds : sleepTime));
+
+            if(isMouseEnter) continue;
+
+            if(isTriggered){
+                isTriggered = false;
+                continue;
+            }
+
+            front();
+        }
+    }
+
+    void mouseenter(){
+        isMouseEnter = true;
+    }
+
+    void mouseleave() {
+        isMouseEnter = false;
+        isTriggered = true;
+    }
 
     void front(){
+        if (current == null) return;
         if(index + 1 >= objects.length) index = 0;
         else index++;
+
+        isTriggered = true;
     }
 
     void back(){
+        if (current == null) return;
         if(index - 1 < 0) index = objects.length - 1;
         else index--;
+
+        isTriggered = true;
     }
 }
 
